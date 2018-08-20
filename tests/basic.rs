66c19f3780c3f688e8bfa9b67db1f0988ee4ea74
apiraino <apiraino@users.noreply.github.com>
2018-08-21 01:37:37 +0200

    add tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -6,7 +6,7 @@ extern crate dotenv_codegen;
 extern crate serde_json;
 
 use reqwest::header::Authorization;
-use reqwest::{Client, Response, StatusCode};
+use reqwest::{Client, StatusCode};
 
 mod common;
 
@@ -29,16 +29,51 @@ fn test_list_users() {
 }
 
 #[test]
-fn test_signup() {
+fn test_signup_ok() {
+    extern crate serde_json;
+    use serde_json::Value;
+
     let api_base_uri = common::api_base_url();
-    let credentials = json!({"username": "antonio", "password": "1234567"});
+    let user_data = json!({
+        "username": "john",
+        "password": "1234567",
+        "email": "hey@email.com"
+    });
     let client = Client::new();
-    let response = client
+    let mut response = client
         .post(api_base_uri.join("/signup").unwrap())
-        .json(&credentials)
+        .json(&user_data)
         .send()
         .unwrap();
+    let resp_str: &str = &response.text().unwrap().to_string();
+    println!("STR {:?}", resp_str);
+    let resp_data: Value = serde_json::from_str(resp_str).unwrap();
+    let user_data: Value = resp_data["user"].clone();
+    println!("JSON {:?}", user_data);
+    let resp_data2: open_taffeta_lib::models::User =
+        serde_json::from_value(user_data.clone()).unwrap();
+    println!("USER {:?}", resp_data2);
     assert_eq!(response.status(), StatusCode::Created);
+
+    let url = &format!("/user/{}", resp_data["user"]["id"]).to_string();
+    response = client
+        .get(api_base_uri.join(url).unwrap())
+        .send()
+        .expect("thought it worked...");
+    assert_eq!(response.status(), StatusCode::Ok);
+    let resp_str: &str = &response.text().unwrap().to_string();
+    let resp_data: Value = serde_json::from_str(resp_str).unwrap();
+    println!("JSON2 {:?}", resp_data["user"][0]["id"]);
+    assert_eq!(resp_data["user"][0]["id"], user_data["id"]);
+
+    response = client
+        .post(api_base_uri.join("/signup").unwrap())
+        .json(&user_data)
+        .send()
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::BadRequest);
+    // let resp_data: Value = serde_json::from_str(resp_str).unwrap();
+    assert_eq!(resp_str.contains("record already exists"), true)
 }
 
 #[test]
